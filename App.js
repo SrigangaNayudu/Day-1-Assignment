8a5ebc77e3befb500eb108d1d79eda449af60e06
SrigangaNayudu <66818571+SrigangaNayudu@users.noreply.github.com>
2021-07-13 14:44:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from "react"
+     "./style.css"
+import Button from "./Button"
+
+const Apple=()=>{
+    return(
+        <div>
+            <h1 style={{textAlign : "center"}}>Hello All!!</h1>
+            <h2>HELLO!! I am Sriganga Nayudu from Andhra Pradesh. i am pursuing bachelor degree with specilization CSE in Kakinada Institute Of Engineering And Techonology at kakinada</h2>
+            <Button buttonText="ABOUT"/>
+            <Button buttonText="EDUCATION"/>
+            <Button buttonText="PROJECTS"/>
+            <Button buttonText="SKILLS"/>
+        </div>
+        
+    )
+}
+
+export default Apple
